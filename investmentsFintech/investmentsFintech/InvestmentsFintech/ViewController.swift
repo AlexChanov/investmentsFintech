2f4915defdcb1a714b5c42fd7817306dd54cfe28
Aleksey Chanov <alekseychanov@gmail.com>
2020-02-20 17:02:05 +0300

    - pods was instaled

---
+++ investmentsFintech/investmentsFintech/InvestmentsFintech/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  InvestmentsFintech
+//
+//  Created by Алексей on 20.02.2020.
+//  Copyright © 2020 Алексей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let view = R.storyboard.launchScreen()
+
+    }
+
+
+}
+
---
+++ investmentsFintech/investmentsFintech/Pods/Target Support Files/Pods-InvestmentsFintech/Pods-InvestmentsFintech-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InvestmentsFintech : NSObject
+@end
+@implementation PodsDummy_Pods_InvestmentsFintech
+@end
